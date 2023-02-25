e87b18924ae2ee375b05dadc52e84509e0338d86
fruitea <aetiurf@gmail.com>
2023-02-25 16:38:34 +0000

    recover /userprog/exception.c

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -138,10 +138,8 @@ static void page_fault(struct intr_frame* f) {
     /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-    printf("%s: exit(-1)\n", thread_current()->pcb->process_name);
-    process_exit();
-    //  printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
-    //         not_present ? "not present" : "rights violation", write ? "writing" : "reading",
-    //         user ? "user" : "kernel");
-    //  kill(f);
+    printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
+           not_present ? "not present" : "rights violation", write ? "writing" : "reading",
+           user ? "user" : "kernel");
+    kill(f);
 }
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,36 +1,52 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "filesys/filesys.h"
 #include "stdio.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "userprog/process.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
 static void syscall_handler(struct intr_frame* f UNUSED) {
-  uint32_t* args = ((uint32_t*)f->esp);
+    uint32_t* args = ((uint32_t*)f->esp);
 
-  /*
+    /*
    * The following print statement, if uncommented, will print out the syscall
    * number whenever a process enters a system call. You might find it useful
    * when debugging. It will cause tests to fail, however, so you should not
    * include it in your final submission.
    */
 
-  /* printf("System call number: %d\n", args[0]); */
+    /* printf("System call number: %d\n", args[0]); */
 
-  if (args[0] == SYS_EXIT) {
-    f->eax = args[1];
-    printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
-    process_exit();
-  } else if (args[0] == SYS_WRITE) {
-    if (args[1] == STDOUT_FILENO) {
-      putbuf((char*)args[2], args[3]);
-    }
-  } else if (args[0] == SYS_PRACTICE) {
-    f->eax = args[1] + 1;
-  }
+    switch (args[0]) {
+        case SYS_HALT:
+            shutdown_power_off();
+            break;
+
+        case SYS_EXIT:
+            f->eax = args[1];
+            printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
+            process_exit();
+            break;
+
+        case SYS_WRITE:
+            if (args[1] == STDOUT_FILENO) {
+                putbuf((char*)args[2], args[3]);
+            }
+            break;
+
+        case SYS_PRACTICE:
+            f->eax = args[1] + 1;
+            break;
+
+        default:
+            printf("System call number: %d", args[0]);
+            process_exit();
+    };
 }
